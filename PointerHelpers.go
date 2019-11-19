5e5d146ba53409f0e0d7e2b7a767f68c42634ca7
Daniel <daniel.unverricht@pentlandfirth.com>
2019-11-19 13:17:30 +0100

    add more pointer helpers

--- PointerHelpers.go
+++ PointerHelpers.go
@@ -13,3 +13,27 @@ func PtrToTime(s time.Time) *time.Time {
 func PtrToBool(s bool) *bool {
 	return &s
 }
+
+func PtrToInt(s int) *int {
+	return &s
+}
+
+func PtrToInt32(s int32) *int32 {
+	return &s
+}
+
+func PtrToInt64(s int64) *int64 {
+	return &s
+}
+
+func PtrToFloat32(s float32) *float32 {
+	return &s
+}
+
+func PtrToFloat64(s float64) *float64 {
+	return &s
+}
+
+func PtrToDuration(s time.Duration) *time.Duration {
+	return &s
+}
