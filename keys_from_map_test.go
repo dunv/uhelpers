0a30bd0a4a7dd08d5c493ae789fb008147ee479d
Daniel Unverricht <daniel@unverricht.net>
2024-05-21 11:53:14 +0200

    generalize keys from map

--- keys_from_map_test.go
+++ keys_from_map_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestKeysFromMap(t *testing.T) {
+func TestKeysFromMapString(t *testing.T) {
 	m := map[string]int{
 		"one":   1,
 		"two":   2,
@@ -15,4 +15,18 @@ func TestKeysFromMap(t *testing.T) {
 	}
 	keys := uhelpers.KeysFromMap(m)
 	require.Equal(t, []string{"one", "two", "three"}, keys)
+}
+
+func TestKeysFromMapCustomType(t *testing.T) {
+	type testType string
+	one := testType("one")
+	two := testType("two")
+	three := testType("three")
+	m := map[testType]int{
+		one:   1,
+		two:   2,
+		three: 3,
+	}
+	keys := uhelpers.KeysFromMap(m)
+	require.Equal(t, []testType{one, two, three}, keys)
 }
